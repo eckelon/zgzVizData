a405a4263bb69aa757a16b54b1bb16e51d471fa2
Eduardo Ramos <eduramiba@gmail.com>
2015-07-11 12:28:44 +0200

    Progress in iescities api usage

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var utils = require("../utils");
+
+var SQL = 'select s.estacion, d.fecha_dt, d.last_modified, d.o3_d, d.no2_d, d.pm10_d, d.so2_d, d.sh2_d, d.co_d from'
++ ' docs as d,'
++ ' docs_estacion_smultiple as s'
++ ' where d.id = s.parent_id'
++ ' limit 100000';
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({prueba: 'cool'});
+router.get('/', function (req, res, next) {
+    var options = {
+        host: 'iescities.com',
+        path: '/IESCities/api/data/query/284/sql?origin=original',
+        method: 'POST',
+        body: SQL
+    };
+    utils.getJSON(options, function(status, data){
+        res.json(data);
+    });
+
 });
 
 module.exports = router;
