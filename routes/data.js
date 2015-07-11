bdd27d570b5d505ef99104b5c52b7f09c88b4053
eckelon <eckelon@gmail.com>
2015-07-11 19:13:21 +0200

    graph is now inside a scrollable div, so it can be actually used. Other small fixes...

--- routes/data.js
+++ routes/data.js
@@ -4,17 +4,18 @@ var router = express.Router();
 var utils = require("../utils");
 
 var SQL = 'select s.estacion, d.fecha_dt, d.last_modified, d.o3_d, d.no2_d, d.pm10_d, d.so2_d, d.sh2_d, d.co_d from'
-        + ' docs as d,'
-        + ' docs_estacion_smultiple as s'
-        + ' where d.id = s.parent_id'
-//+ " and fecha_dt >= '2014-01-01' and fecha_dt < '2015-01-01'"
-        + ' order by fecha_dt'
-        ;
++ ' docs as d,'
++ ' docs_estacion_smultiple as s'
++ ' where d.id = s.parent_id and '
++ " fecha_dt >= '2014-01-01' and fecha_dt < '2015-01-01'"
++ ' order by fecha_dt'
++ ' limit 10000'
+;
+
 
 var IESCITIES_DATASET_ID = 288;
 
-var NodeCache = require("node-cache");
-var cache = new NodeCache({stdTTL: 60 * 10, checkperiod: 60});
+var dataCache = null;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -22,23 +23,21 @@ router.get('/', function (req, res, next) {
         res.json(data);
     }
 
-    cache.get('data', function (err, value) {
-        if (value) {
-            generateResponse(value);
-        } else {
-            var options = {
-                host: 'iescities.com',
-                path: '/IESCities/api/data/query/' + IESCITIES_DATASET_ID + '/sql?origin=original',
-                method: 'POST',
-                body: SQL
-            };
-            utils.getJSON(options, function (status, data) {
-                cache.set('data', data);
-                generateResponse(data);
-            });
-        }
-    });
-
+    if(!dataCache){
+        var options = {
+            host: 'iescities.com',
+            path: '/IESCities/api/data/query/' + IESCITIES_DATASET_ID + '/sql?origin=original',
+            method: 'POST',
+            body: SQL
+        };
+        utils.getJSON(options, function(status, data){
+            dataCache = data;
+            generateResponse(data);
+        });
+    }else{
+        console.log('ok');
+        generateResponse(dataCache);
+    }
 });
 
 module.exports = router;
