7a8a3ad940587ee58fa03f8932398aa5f3e095a5
Eduardo Ramos <eduramiba@gmail.com>
2015-07-11 19:30:05 +0200

    Progress in charts

--- public/js/parallel-coords.js
+++ public/js/parallel-coords.js
@@ -1,13 +1,6 @@
 /* global d3 */
 
 $(document).ready(function () {
-    $.ajax({
-        url: '/data',
-        type: 'GET',
-        dataType: 'JSON',
-        success: initParallelCoordinates
-    });
-
     var monthNames = [
         'Enero',
         'Febrero',
@@ -23,98 +16,129 @@ $(document).ready(function () {
         'Diciembre'
     ];
     
-    var dimensions = {
-        station: 'Estación',
-        year: 'Año',
-        month: 'Mes',
-        day: 'Día',
-        o3: 'O\u2083',
-        so2: 'SO\u2082',
-        no2: 'NO\u2082',
-        co: 'CO',
-        pm10: 'PM\u2081\u2080',
-        sh2: 'SH\u2082'
-    };
+    var $container = $("#parallel-container");
+    var $applyButton = $("#apply");
+    var $startDate = $("#startDate");
+    var $endDate = $("#endDate");
     
-    var dimensionNames = [];
-
-    var dataTypes = {};
-
-    //All numbers by default:
-    var props = Object.getOwnPropertyNames(dimensions);
-    for (var i in props) {
-        var name = dimensions[props[i]];
+    $applyButton.click(createChart);
+    
+    var datepickerFormat = d3.time.format("%Y-%m-%d");
+    
+    var today = new Date();
+    var todayMinus1Year = new Date(today - 86400 * 1000 * 365);
+    
+    $endDate.val(datepickerFormat(today));
+    $startDate.val(datepickerFormat(todayMinus1Year));//About one year before...
+    
+    function createChart(){
+        $container.html('');
         
-        dataTypes[name] = 'number';
-        dimensionNames.push(name);
-    }
-
-    dataTypes[dimensions.station] = 'string';
-
-    var timeFormat = d3.time.format('%Y-%m-%dT%H:%M:%SZ');
-    var yearPartFormat = d3.time.format('%Y');
-    var monthPartFormat = d3.time.format('%m');
-    var dayPartFormat = d3.time.format('%d');
-
-    function contaminantToNumber(d) {
-        if (d === null || typeof d === 'object') {
-            return undefined;
-        } else {
-            return Number(d);
-        }
-    }
-
-    function getMonthFromDate(date) {
-        return Number(monthPartFormat(date));
-        return monthNames[Number(monthPartFormat(date)) - 1];
-    }
-
-    function initParallelCoordinates(data) {
-        var parallelsData = [
-        ];
-        
-        for (var i = 0, max = data.rows.length; i < max; i++) {
-            var row = data.rows[i];
-
-            var date = timeFormat.parse(row.fecha_dt);
-
-            var parallelRow = {};
-
-            parallelRow[dimensions.station] = row.estacion;
-            parallelRow[dimensions.year] = Number(yearPartFormat(date));
-            parallelRow[dimensions.month] = getMonthFromDate(date);
-            parallelRow[dimensions.day] = Number(dayPartFormat(date));
-            parallelRow[dimensions.o3] = contaminantToNumber(row.o3_d);
-            parallelRow[dimensions.so2] = contaminantToNumber(row.so2_d);
-            parallelRow[dimensions.no2] = contaminantToNumber(row.no2_d);
-            parallelRow[dimensions.co] = contaminantToNumber(row.co_d);
-            parallelRow[dimensions.pm10] = contaminantToNumber(row.pm10_d);
-            parallelRow[dimensions.sh2] = contaminantToNumber(row.sh2_d);
-
-            parallelsData.push(parallelRow);
-        }
-        
-        var colorScale = d3.scale.category10();
-
-        var pc = d3.parcoords()("#parallel-container")
-                .data(parallelsData)
-                .types(dataTypes)
-                .dimensions(dimensionNames)
-                .margin({top: 24, left: 100, bottom: 12, right: 0})
-                .mode("queue")
-                .autoscale()
-                .alpha(0.2)
-                .color(function(d){
-                    return colorScale(d[dimensions.station]);
-                })
-                .render()
-                .brushMode("1D-axes")  // enable brushing
-                .reorderable()
-                ;
-                
-                
-        pc.on('brush', function(){
-            console.log(pc.brushed().length);
+        $.ajax({
+            url: '/data',
+            data: {
+                start: $startDate.val(),
+                end: $endDate.val()
+            },
+            type: 'GET',
+            dataType: 'JSON',
+            success: initParallelCoordinates
         });
+
+
+        var dimensions = {
+            station: 'Estación',
+            year: 'Año',
+            month: 'Mes',
+            day: 'Día',
+            o3: 'O\u2083',
+            so2: 'SO\u2082',
+            no2: 'NO\u2082',
+            co: 'CO',
+            pm10: 'PM\u2081\u2080',
+            sh2: 'SH\u2082'
+        };
+
+        var dimensionNames = [];
+
+        var dataTypes = {};
+
+        //All numbers by default:
+        var props = Object.getOwnPropertyNames(dimensions);
+        for (var i in props) {
+            var name = dimensions[props[i]];
+
+            dataTypes[name] = 'number';
+            dimensionNames.push(name);
+        }
+
+        dataTypes[dimensions.station] = 'string';
+
+        var timeFormat = d3.time.format('%Y-%m-%dT%H:%M:%SZ');
+        var yearPartFormat = d3.time.format('%Y');
+        var monthPartFormat = d3.time.format('%m');
+        var dayPartFormat = d3.time.format('%d');
+
+        function contaminantToNumber(d) {
+            if (d === null || typeof d === 'object') {
+                return undefined;
+            } else {
+                return Number(d);
+            }
+        }
+
+        function getMonthFromDate(date) {
+            return Number(monthPartFormat(date));
+            //return monthNames[Number(monthPartFormat(date)) - 1];
+        }
+
+        function initParallelCoordinates(data) {
+            var parallelsData = [
+            ];
+
+            for (var i = 0, max = data.rows.length; i < max; i++) {
+                var row = data.rows[i];
+
+                var date = timeFormat.parse(row.fecha_dt);
+
+                var parallelRow = {};
+
+                parallelRow[dimensions.station] = row.estacion;
+                parallelRow[dimensions.year] = Number(yearPartFormat(date));
+                parallelRow[dimensions.month] = getMonthFromDate(date);
+                parallelRow[dimensions.day] = Number(dayPartFormat(date));
+                parallelRow[dimensions.o3] = contaminantToNumber(row.o3_d);
+                parallelRow[dimensions.so2] = contaminantToNumber(row.so2_d);
+                parallelRow[dimensions.no2] = contaminantToNumber(row.no2_d);
+                parallelRow[dimensions.co] = contaminantToNumber(row.co_d);
+                parallelRow[dimensions.pm10] = contaminantToNumber(row.pm10_d);
+                parallelRow[dimensions.sh2] = contaminantToNumber(row.sh2_d);
+
+                parallelsData.push(parallelRow);
+            }
+
+            var colorScale = d3.scale.category10();
+
+            var pc = d3.parcoords()("#parallel-container")
+                    .data(parallelsData)
+                    .types(dataTypes)
+                    .dimensions(dimensionNames)
+                    .margin({top: 24, left: 100, bottom: 12, right: 0})
+                    .mode("queue")
+                    .autoscale()
+                    .alpha(0.2)
+                    .color(function(d){
+                        return colorScale(d[dimensions.station]);
+                    })
+                    .render()
+                    .brushMode("1D-axes")  // enable brushing
+                    .reorderable()
+                    ;
+
+
+            pc.on('brush', function(){
+                console.log(pc.brushed().length);
+            });
+        }
     }
 });
--- routes/data.js
+++ routes/data.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 var utils = require("../utils");
 
+var DATE_COND_PLACEHOLDER = '{DATE_COND_PLACEHOLDER}';
 var SQL = 'select s.estacion, d.fecha_dt, d.last_modified, d.o3_d, d.no2_d, d.pm10_d, d.so2_d, d.sh2_d, d.co_d from'
         + ' docs as d,'
         + ' docs_estacion_smultiple as s'
         + ' where d.id = s.parent_id'
+        + ' ' + DATE_COND_PLACEHOLDER
 //+ " and fecha_dt >= '2014-01-01' and fecha_dt < '2015-01-01'"
         + ' order by fecha_dt'
         ;
+        
+var DEFAULT_START_DATE = '2014-01-01';
 
 var IESCITIES_DATASET_ID = 288;
 
@@ -21,7 +26,27 @@ router.get('/', function (req, res, next) {
     function generateResponse(data){
         res.json(data);
     }
-
+    
+    var startDate = req.param('start');
+    var endDate = req.param('end');
+    
+    if(!moment(startDate, "YYYY-MM-DD").isValid()){
+        startDate = DEFAULT_START_DATE;
+    }
+    if(!moment(endDate, "YYYY-MM-DD").isValid()){
+        endDate = null;
+    }
+    
+    var finalSQL = SQL;
+    
+    if(startDate && endDate){
+        finalSQL = finalSQL.replace(DATE_COND_PLACEHOLDER, "and fecha_dt between '" + startDate +"' AND '" + endDate + "'");
+    }else{
+        finalSQL = finalSQL.replace(DATE_COND_PLACEHOLDER, "and fecha_dt >= '" + startDate);
+    }
+    
+    console.log(finalSQL);
+    
     cache.get('data', function (err, value) {
         if (value) {
             generateResponse(value);
@@ -30,10 +55,13 @@ router.get('/', function (req, res, next) {
                 host: 'iescities.com',
                 path: '/IESCities/api/data/query/' + IESCITIES_DATASET_ID + '/sql?origin=original',
                 method: 'POST',
-                body: SQL
+                body: finalSQL
             };
             utils.getJSON(options, function (status, data) {
-                cache.set('data', data);
+                if(data.rows && data.rows.length){
+                    cache.set('data', data);
+                }
+                
                 generateResponse(data);
             });
         }
